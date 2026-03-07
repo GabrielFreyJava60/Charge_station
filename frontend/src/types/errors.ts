6b056233f788028510c6afe1a24581984577ae78
srgsuh <srg.suhoverhov@gmail.com>
2026-03-07 11:06:11 +0200

    Add errors hierarchy

--- frontend/src/types/errors.ts
+++ frontend/src/types/errors.ts
@@ -1,12 +1,36 @@
-export class ApiClientError extends Error {
-    name: string;
+
+export class ApiError extends Error {
     code: string;
+    constructor(message: string, code: string) {
+        super(message);
+
+        Object.setPrototypeOf(this, new.target.prototype);
+
+        this.name = 'ApiError'; 
+        this.code = code;
+    }
+}
+
+export class HttpError extends ApiError {
     status?: number;
 
-    constructor(message: string, code: string, status?: number) {
-        super(message);
-        this.name = "ApiClientError";
-        this.code = code;
+    constructor(message: string, code: string, status: number) {
+        super(message, code);
+        this.name = 'HttpError';
         this.status = status;
     }
 };
+
+export class UnauthorizedError extends HttpError {
+    constructor(message: string) {
+        super(message, 'UNAUTHORIZED', 401);
+        this.name = 'UnauthorizedError';
+    }
+};
+
+export class ForbiddenError extends HttpError {
+    constructor(message: string) {
+        super(message, 'FORBIDDEN', 403);
+        this.name = 'ForbiddenError';
+    }
+}
