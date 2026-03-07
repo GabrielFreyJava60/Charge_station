1720c9ada64dfaf3d618dd683bb50cbd6692c7e7
srgsuh <srg.suhoverhov@gmail.com>
2026-03-07 16:56:06 +0200

    Add API_PREFIX and health related parameters

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -2,5 +2,7 @@
 export const config = {
     logLevel: import.meta.env.VITE_LOG_LEVEL,
     apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+    apiPrefix: import.meta.env.VITE_API_URL_PREFIX,
     apiTimeout: import.meta.env.VITE_API_TIMEOUT,
+    apiHealthPath: import.meta.env.VITE_API_HEALTH_PATH,
 }
