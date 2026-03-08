88e2ce5b2f023937d4cbb30eaf4b988908e5c27f
srgsuh <srg.suhoverhov@gmail.com>
2026-03-08 18:44:15 +0200

    Add guest dashboard

---
+++ frontend/src/pages/guest/GuestDashboardPage.tsx
@@ -0,0 +1,17 @@
+import HealthChecker from '@/components/HealthChecker';
+import WelcomeTable from '@/components/WelcomeTable'
+
+const GuestDashboardPage = () => {
+  return (
+    <>
+      <div>
+        Welcome to the Charging stations application
+        <HealthChecker defaultInfo="Click to check!" endpoint='/health' checkerName='Check backend service'/>
+        <HealthChecker defaultInfo="Click to check!" endpoint='/health/api' checkerName='Check backend + lambda'/>
+        <WelcomeTable />
+      </div>
+    </>
+  )
+}
+
+export default GuestDashboardPage;
