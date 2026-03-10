f9be94807bc8d96e6eefe08f6943eb586e1f9276
srgsuh <srg.suhoverhov@gmail.com>
2026-03-10 15:42:18 +0200

    Implement basic login procedure

--- frontend/src/pages/guest/LoginPage.tsx
+++ frontend/src/pages/guest/LoginPage.tsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 
 const LoginPage = () => {
+    const [isRegister, setIsRegister] = useState<boolean>(false);
+    const [email, setEmail] = useState<string | undefined>(undefined);
+    const [password, setPassword] = useState<string | undefined>(undefined);
+    const [confirmPassword, setConfirmPassword] = useState<string | undefined>(undefined);
     return (
+        <div className="loginForm">
+        <h1>LOGIN PAGE</h1>
+        <h4>
+            {isRegister ? "Sign up to create an account" : "Sign in to your account"}
+        </h4>
+        <form onSubmit={isRegister ? handleSignUp : handleSignIn}>
         <div>
-            <h1>LOGIN PAGE</h1>
+          <input
+            className="inputText"
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
         </div>
+        <div>
+          <input
+            className="inputText"
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        {isRegister && (
+          <div>
+            <input
+              className="inputText"
+              id="confirmPassword"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+        )}
+        <button type="submit">{isRegister ? "Sign Up" : "Sign In"}</button>
+      </form>
+      <button type="button" onClick={() => setIsRegister(!isRegister)}>
+        {isRegister
+          ? "Already have an account? Sign In"
+          : "Need an account? Sign Up"}
+      </button>
+    </div>
     );
 };
 
