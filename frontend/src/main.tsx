39a2fbcd7718c07c7e640ee32eaa44c53951d5af
srgsuh <srg.suhoverhov@gmail.com>
2026-03-10 23:19:18 +0200

    Add authProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import { RouterProvider } from 'react-router';
 import './index.css';
 import { getLogger } from '@/services/logging';
 import router from './router/router.tsx';
+import { AuthProvider } from './auth/AuthProvider.tsx';
 
 const logger = getLogger();
 logger.info("Application bootstrap");
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
