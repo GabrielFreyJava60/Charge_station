bcab00ef3e70d6e0659b08d29f2e978dd7b67f51
srgsuh <srg.suhoverhov@gmail.com>
2026-03-11 21:32:03 +0200

    Create MeChecker and add it to Dashboard pages

--- frontend/src/pages/admin/AdminDashboardPage.tsx
+++ frontend/src/pages/admin/AdminDashboardPage.tsx
@@ -1,6 +1,11 @@
+import MeChecker from "@/components/MeChecker";
+
 const AdminDashboardPage = () => {
   return (
-    <div><h1>ADMIN DASHBOARD PAGE</h1></div>
+    <div>
+      <h1>ADMIN DASHBOARD PAGE</h1>
+      <MeChecker />
+    </div>
   );
 };
 
--- frontend/src/pages/user/UserDashboardPage.tsx
+++ frontend/src/pages/user/UserDashboardPage.tsx
@@ -1,8 +1,12 @@
+import MeChecker from "@/components/MeChecker";
 
 const UserDashboardPage = () => {
   return (
-    <div><h1>USER DASHBOARD PAGE</h1></div>
-  )
+    <div>
+      <h1>User DASHBOARD PAGE</h1>
+      <MeChecker />
+    </div>
+  );
 }
 
-export default UserDashboardPage
+export default UserDashboardPage;
